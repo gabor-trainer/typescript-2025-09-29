07f085c17ec2792d02cbb52754512a29742b80c5
github will ignore this <gabor.trainer@outlook.com>
2025-09-29 13:09:53 +0200

    feat: update VSCode configuration files for improved development experience

--- starter-project/src/index.ts
+++ starter-project/src/index.ts
@@ -1,4 +1,5 @@
-// src/index.ts
+// the following line is a smoke test to verify the linter is working, must be red.
+// change var -> const and remove the comment to fix it.
 var greeting = "Hello, Professional TypeScript!"
 
 function showGreeting(msg: string) {
