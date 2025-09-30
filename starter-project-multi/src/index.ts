cb6ecd9baa200efacf7b053758a904171516638c
github will ignore this <gabor.trainer@outlook.com>
2025-09-30 08:56:08 +0200

    feat: add multi seed project

---
+++ starter-project-multi/src/index.ts
@@ -0,0 +1,11 @@
+// the following line is a smoke test to verify the linter is working, must be red.
+// change var -> const and remove the comment to fix it.
+const greeting = "Hello, Professional TypeScript!"
+
+function showGreeting(msg: string) {
+    console.log(msg)
+}
+
+showGreeting(greeting);
+
+export { };
